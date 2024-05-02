26c88a2412977b1faf009a4cb87b693e0850872d
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-05-02 13:46:04 -0400

    Time: 53 ms (67.63%), Space: 16.7 MB (81.93%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,11 +1,43 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        dp = [0] * (len(cost) + 1)
+        n = len(cost)
 
-        # Recursive Recurrence
-        for i in range(2, len(cost) + 1):
-            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+        # Initialize the dp array
+        dp = [0] * (n + 1)
+
+        # Base Cases are implicit
+        dp[0] = 0
+        dp[1] = 0
+
+        # Recurrence relation
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
         
         return dp[-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # dp = [0] * (len(cost) + 1)
+
+        # # Recursive Recurrence
+        # for i in range(2, len(cost) + 1):
+        #     dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+        
+        # return dp[-1]
 
