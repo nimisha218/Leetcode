87eb278a8df9302ed08202eb63ecc775e5346677
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-06-05 15:19:56 -0400

    Time: 81 ms (5.22%), Space: 16.4 MB (95.45%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        freq = defaultdict(int)
+        count = 0
+        found = []
+
+        for i in range(len(nums)):
+            freq[nums[i]] += 1
+            if freq[nums[i]] > 2:
+                count += 1
+                found.append(i)
+                # This is a problem
+                nums[i] = "_"
+        
+        ans = len(nums) - count
+
+        while count > 0:
+            # Find the location of the first "_"
+            for i in range(len(nums)):
+                if nums[i] == "_":
+                    pos = i
+                    break
+            
+            # i -> Current position of "_"
+            # Swap i and i + 1
+            while i < len(nums) - 1:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                i += 1
+            
+            count -= 1
+    
+        return ans
+                
