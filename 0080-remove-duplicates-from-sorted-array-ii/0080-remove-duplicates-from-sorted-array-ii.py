f27ea72e55d42b98be2dee1560eefc1ab0de9001
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-06-06 13:38:31 -0400

    Time: 78 ms (5.20%), Space: 16.5 MB (67.51%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -29,7 +29,6 @@ class Solution:
             while i < len(nums) - 1:
                 nums[i], nums[i+1] = nums[i+1], nums[i]
                 i += 1
-            
             count -= 1
     
         return ans
