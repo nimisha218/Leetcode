1cafede0b7a0f1a97d2db227f81c6953e1b9ab2a
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-06-15 09:25:40 -0400

    Time: 512 ms (30.52%), Space: 19.6 MB (63.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -3,35 +3,35 @@ from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         
-        # Check if the current position is valid or not
         def isValid(row, col):
-            return 0 <= row < num_rows and 0 <= col < num_cols and mat[row][col] == 1
+            return 0 <= row < num_rows and 0 <= col < num_cols
         
-        # Get the number of rows and columns
         num_rows = len(mat)
         num_cols = len(mat[0])
-        
+
         queue = deque()
         seen = set()
-        
+
         for row in range(num_rows):
             for col in range(num_cols):
                 if mat[row][col] == 0:
                     queue.append((row, col, 1))
+                    seen.add((row, col))
         
-        # Define the directions list
-        directions = [ (0, 1), (1, 0), (-1, 0), (0, -1) ]
-        
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
         while queue:
-            
+
             row, col, steps = queue.popleft()
-            for i, j in directions:
-                next_row = i + row
-                next_col = j + col
-                if (next_row, next_col) not in seen and isValid(next_row, next_col):
-                    if mat[next_row][next_col] == 1:
-                        mat[next_row][next_col] = steps
-                    seen.add((next_row, next_col))
-                    queue.append((next_row, next_col, steps + 1))
+
+            for dx, dy in directions:
+                new_row = row + dx
+                new_col = col + dy
+                if isValid(new_row, new_col) and (new_row, new_col) not in seen:
+                    queue.append((new_row, new_col, steps + 1))
+                    seen.add((new_row, new_col))
+                    mat[new_row][new_col] = steps
         
         return mat
+
+    
