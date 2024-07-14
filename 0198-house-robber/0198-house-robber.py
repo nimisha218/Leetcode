e66aa89cf53b428ed01402e4b965c8d456027b4f
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-07-14 10:21:24 -0400

    Time: 34 ms (72.03%), Space: 16.7 MB (6.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,19 @@
+from functools import cache
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
-        if len(nums) == 1:
-            return nums[0]
-        
-        # Initialize the dp array
-        dp = [0] * len(nums)
+        @cache
+        def dp(i):
 
-        # Base cases - we can rob the first two houses independently
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
-        # Recurrence relation
-        for i in range(2, len(nums)):
-            # We can rob this house and add the cost of i - 2 th house
-            # or, we can choose to skip to rob this house, and just include the i - 1 th house
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+            if i == 0:
+                return nums[i]
+            
+            elif i == 1:
+                return max(nums[0], nums[1])
+            
+            else:
+                return max(dp(i-2) + nums[i], dp(i-1))
         
-        return dp[-1]
+        return dp(len(nums) - 1)
 
