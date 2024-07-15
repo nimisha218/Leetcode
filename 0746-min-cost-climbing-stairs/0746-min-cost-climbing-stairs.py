80f21a8814298896e980dccbe87924014133c31e
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-07-15 15:00:58 -0400

    Time: 66 ms (6.21%), Space: 19.7 MB (5.30%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,21 +1,17 @@
+from functools import cache
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        n = len(cost)
+        @cache
+        def dp(i):
 
-        # Initialize the dp array
-        dp = [0] * (n + 1)
-
-        # Base Cases are implicit
-        dp[0] = 0
-        dp[1] = 0
-
-        # Recurrence relation
-        for i in range(2, n + 1):
-            dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
+            if i == 0 or i == 1:
+                return 0
+            
+            return min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
         
-        return dp[-1]
-
+        return dp(len(cost))
 
 
 
