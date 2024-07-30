a0d56c1c5ddb88b0b42f4b5c0bd938703edea186
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-07-30 10:42:34 -0400

    Time: 37 ms (40.73%), Space: 16.6 MB (30.44%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -3,22 +3,21 @@ from collections import defaultdict
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         
-
-        d = defaultdict(int)
-
         s = s.split(" ")
-
+        
         if len(pattern) != len(s):
             return False
-        
-        for i in range(len(pattern)):
 
-            if pattern[i] not in d:
+        d = defaultdict(str)
+        
+        for i in range(len(s)):
+            if pattern[i] in d:
+                if d[pattern[i]] != s[i]:
+                    return False
+            else:
                 if s[i] in d.values():
                     return False
                 d[pattern[i]] = s[i]
-            else:
-                if d[pattern[i]] != s[i]:
-                    return False
         
         return True
+            
