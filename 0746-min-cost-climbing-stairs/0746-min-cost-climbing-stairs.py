e59c686079400b722e7893a2364214acfd4e5189
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-09-03 10:50:33 -0400

    Time: 61 ms (18.53%), Space: 19.7 MB (7.62%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -3,22 +3,15 @@ from functools import cache
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        # Top-down approach (Start from the result case and work your way DOWN to the base case)
-
+        # Top-Down Approach
+        @cache
         def dp(i):
-            
-            # Base Case -> Can start from index 0 or 1
+
             if i <= 1:
                 return 0
             
-            # Check if the state was already computed
-            if i in memo:
-                return memo[i]
-            
-            # Define the recurrence relation
-            memo[i] = min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
-            return memo[i]
-        
-        memo = {}
+            return min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])   
+                 
         return dp(len(cost))
+            
 
