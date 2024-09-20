58038df360112a937ccb9c835a742c6a42b69de4
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-09-20 18:07:45 -0400

    Time: 1929 ms (18.15%), Space: 17 MB (13.29%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,18 +2,46 @@ from functools import cache
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+
+        dp = [1] * len(nums)
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+
+                if nums[j] > nums[i]:
+                    dp[j] = max(dp[j], dp[i] + 1)
         
-        @cache
-        def dp(i):
+        return max(dp)
 
-            # Base Case
-            ans = 1
 
+
+
+
+
+
+
+        
+        # Top-down approach
+
+        # @cache
+        # def dp(i):
+
+        #     ans = 1
+
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             ans = max(ans, dp(j) + 1)
+            
+        #     return ans
+        
+        # return max(dp(i) for i in range(len(nums)))
+
+        # Bottom-up implementation
+        dp = [1] * len(nums)
+
+        for i in range(len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
-                    ans = max(ans, dp(j) + 1)
-                
-            return ans
-
-        return max(dp(i) for i in range(len(nums)))
-    
+                    dp[i] = max(dp[i], 1 + dp[j])
+        
+        return max(dp)
