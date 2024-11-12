418493992d30989aeb17c131275abbfbf3f23089
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-11-12 16:35:51 -0500

    Time: 4 ms (61.04%), Space: 16.6 MB (76.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,42 +3,41 @@ from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        num_rows = len(board)
-        num_cols = len(board[0])
+        n = len(board)
 
-        sections = defaultdict(set)
-
-        # Check for columns
-        for row in range(num_rows):
+        # Check for cols first
+        cols = set()
+        for row in range(n):
             cols = set()
-            for col in range(num_cols):
-                if board[row][col] == '.':
+            for col in range(n):
+                if board[row][col] == ".":
                     continue
                 if board[row][col] in cols:
                     return False
                 cols.add(board[row][col])
         
-        # Check for rows
-        for col in range(num_cols):
+        # Check for rows next
+        for col in range(n):
             rows = set()
-            for row in range(num_rows):
-                if board[row][col] == '.':
+            for row in range(n):
+                if board[row][col] == ".":
                     continue
                 if board[row][col] in rows:
                     return False
                 rows.add(board[row][col])
-
+            
         # Check for sections
-        for row in range(num_rows):
-            for col in range(num_cols):
-
-                if board[row][col] == '.':
+        sections = defaultdict(set)
+        for row in range(n):
+            for col in range(n):
+                if board[row][col] == ".":
                     continue
-                
                 if board[row][col] in sections[(row // 3, col // 3)]:
                     return False
-                
-                sections[(row//3, col//3)].add(board[row][col])
-        
-
+                sections[(row // 3, col // 3)].add(board[row][col])
+            
         return True
+
+
+
+        
