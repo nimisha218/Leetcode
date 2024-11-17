2af3d34dd9ead778f72acf6806794bc2ab20be9c
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-11-17 14:11:17 -0500

    Time: 83 ms (33.05%), Space: 16.7 MB (24.73%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,22 +5,19 @@ class Solution:
 
         result = 0
 
+        d = defaultdict(int)
+
         left = 0
 
-        count = defaultdict(int)
-
         for right in range(len(s)):
+            
+            # Add the character to the freq hash map
+            d[s[right]] += 1
 
-            count[s[right]] += 1
-
-            while (right-left+1) - max(count.values()) > k:
-
-                count[s[left]] -= 1
+            while (right - left + 1 - max(d.values())) > k:
+                d[s[left]] -= 1
                 left += 1
             
             result = max(result, right - left + 1)
         
         return result
-
-        
-        
