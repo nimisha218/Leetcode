0f6f30c1830c5a3410be5ae0d6284ca4535c8f4f
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2024-12-01 10:38:00 -0500

    Time: 0 ms (100.00%), Space: 17.4 MB (6.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,34 +1,35 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
 
-        # Define the backtrack method
         def backtrack(curr, index):
 
-            # Base case
             if index == len(digits):
-                ans.append("".join(curr[:]))
+                result.append("".join(curr[:]))
                 return
-
+            
             for letter in phone[int(digits[index])]:
                 curr.append(letter)
                 backtrack(curr, index + 1)
                 curr.pop()
-        
-        # Populate our digit -> letter hash map
-        phone = {
-            1: [],
-            2: ['a', 'b', 'c'],
-            3: ['d', 'e', 'f'],
-            4: ['g', 'h', 'i'],
-            5: ['j', 'k', 'l'],
-            6: ['m', 'n', 'o'],
-            7: ['p', 'q', 'r', 's'],
-            8: ['t', 'u', 'v'],
-            9: ['w', 'x', 'y', 'z']
-        }
 
-        if digits:
-            ans = []
-            backtrack([], 0)
-            return ans
-        return []
+
+        phone = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+        } 
+
+        if len(digits) == 0:
+            return []
+        
+        result = []
+        backtrack([], 0)
+        return result
+       
+            
+        
